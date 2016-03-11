ec540953e9b0b1bbf3d37934e1581d15dd4a6a49
JosefineK <josefinekorzen@hotmail.com>
2016-03-11 21:56:52 +0100

    Update 01_string.js

--- 201602_introduction/exercises/basic/01_string.js
+++ 201602_introduction/exercises/basic/01_string.js
@@ -1,2 +1,5 @@
 // Create a function that returns the initials of a person
 // HINT: .charAt(index) lets you treat a string like an array of characters
+Initials(firstName,lastName){
+  console.log(firstName.charAt(1)+lastName.charAt(1))
+}
