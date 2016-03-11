bafde84f03970e19809890c39a08c9b3ff6f80f5
JosefineK <josefinekorzen@hotmail.com>
2016-03-11 22:09:16 +0100

    Update 02_number.js

--- 201602_introduction/exercises/basic/02_number.js
+++ 201602_introduction/exercises/basic/02_number.js
@@ -1,3 +1,27 @@
 // Build a small calculator function
 // BONUS: Learn how to round/up down decimals
 // HINT: Learn how to use the switch,case
+Calc(sign,arg1,arg2){
+  switch(sign){
+    case +:
+      console.log(arg1+arg2)
+      break;
+      
+    case -:
+      console.log(arg1-arg2)
+      break;
+    case *:
+      console.log(arg1*arg2)
+      break;
+    case /:
+      if(arg2 !=0){
+      console.log(arg1/arg2)
+      }else{
+        console.log("Can't divdide by zero")
+        break;
+      }
+    default:
+      console.log("awesome calculator function")
+      
+  }
+}
